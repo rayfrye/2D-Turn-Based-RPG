4405489076c86873063e807e45ac83b3c5efdb38
rayfrye <rfrye09@gmail.com>
2015-04-25 08:53:44 -0400

    Build 17
    Created building interior (Test Interior) and wood floor.

---
+++ Assets/Scripts/Classes/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dialogue : MonoBehaviour 
+{
+	public int id;
+
+	public List<string> dialogueStepText = new List<string>();
+	public List<string[]> dialogueOptions = new List<string[]>();
+	public List<string[]> dialogueActions = new List<string[]>();
+
+}
---
+++ Assets/Scripts/Classes/Quest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Quest : MonoBehaviour 
+{
+	public int id;
+
+	public string name;
+	public string description;
+
+	public List<Quest> questPrereqs = new List<Quest>();
+
+	public bool checkifComplete()
+	{
+		bool isComplete = true;
+
+		foreach (Quest questPrereq in questPrereqs) 
+		{
+			if(!questPrereq.isComplete)
+			{
+				isComplete = false;
+			}
+		}
+
+		return isComplete;
+	}
+
+	public bool isComplete;
+}
