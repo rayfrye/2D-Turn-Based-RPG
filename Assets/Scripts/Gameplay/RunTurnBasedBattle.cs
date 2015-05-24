e5126ec44a45bc5ef25f597a352761c37fabd247
Ray Frye <workingversions@gmail.com>
2015-05-23 23:38:38 -0400

    Build 31
    Updates to RunTurnBasedBattle

--- Assets/Scripts/Gameplay/RunTurnBasedBattle.cs
+++ Assets/Scripts/Gameplay/RunTurnBasedBattle.cs
@@ -9,6 +9,45 @@ public class RunTurnBasedBattle : MonoBehaviour
 	public List<GameObject> playerParty = new List<GameObject>();
 	public List<GameObject> enemyParty = new List<GameObject>();
 
+	public enum battleState
+	{
+		PickingAction
+		,PickingTarget
+		,PerformingAction
+	}
+
+	public battleState currentState;
+
+	public List<GameObject> allParties()
+	{
+		List<GameObject> allParties = playerParty;
+
+		allParties.AddRange(enemyParty);
+
+		return allParties;
+	}
+
+	void Update()
+	{
+		runBattle ();
+	}
+
+	void runBattle()
+	{
+		switch(currentState)
+		{
+		case battleState.PickingAction:
+		{
+
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+
 
 
 }
